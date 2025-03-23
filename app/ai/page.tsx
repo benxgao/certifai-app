71f1a61e23232a6164e5b67d58f1904b76104a2a
Xingbin Gao <xingbingao@gmail.com>
2025-03-23 18:13:48 +1300

    Init routes

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,8 +1,36 @@
-'use client';
-
 import React from 'react';
 
-const AIPage = ({ data }) => {
+const fetchData = async (data) => {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/ai`, {
+      cache: 'no-store',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log(`process.env.NEXT_PUBLIC_API_URL: ${process.env.NEXT_PUBLIC_API_URL}`);
+
+    if (!response.ok) {
+      // const error = await response.json();
+      console.error(`fetch error: ${JSON.stringify(response.status)}`);
+      throw new Error(`Failed to fetch data: ${JSON.stringify(response.body)}`);
+    }
+
+    const result = await response.json();
+    console.log(`fetch result: ${JSON.stringify(result)}`);
+    return result;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+const AIPage = async ({}) => {
+  const data = await fetchData({ message: 'Hello, AI!' });
+
   return (
     <div>
       <h1>AI Page</h1>
