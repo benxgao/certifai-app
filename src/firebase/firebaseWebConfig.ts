9231e8a1f277eb48196ca090759c23a41d24eec7
Xingbin <xingbingao@gmail.com>
2025-05-28 22:40:15 +1200

    Enable AI Logic

--- src/firebase/firebaseWebConfig.ts
+++ src/firebase/firebaseWebConfig.ts
@@ -1,5 +1,6 @@
 import { getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getAI, getGenerativeModel, GoogleAIBackend } from 'firebase/ai';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,5 +14,20 @@ const firebaseConfig = {
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
+
+export const firebaseAi = getAI(app, {
+  backend: new GoogleAIBackend(),
+});
+
+export const aiModel = (jsonSchema: any) =>
+  getGenerativeModel(firebaseAi, {
+    model: 'gemini-2.5-flash',
+    generationConfig: jsonSchema
+      ? {
+          responseMimeType: 'application/json',
+          responseSchema: jsonSchema,
+        }
+      : {},
+  });
 
 export default app;
